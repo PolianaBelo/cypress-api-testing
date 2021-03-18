72e77c79a6d2c710e55df4cb9e9544fc8cc7eb64
PolianaBelo <poli.vieira.belo@gmail.com>
2021-03-18 11:58:26 -0300

    refactoring to use fixtures in location test flows

--- cypress/integration/location_test/postLocation.spec.js
+++ cypress/integration/location_test/postLocation.spec.js
@@ -3,42 +3,39 @@ import "cypress-localstorage-commands";
 describe('Create Location 201', () => {
 
     Cypress.config('baseUrl', 'https://api.fidel.uk/v1');
+    let authPayload;
+    let locationRequestData;
 
-    let requestData = {
-        // pathParam: localStorage.getItem('programId'),
-        pathParam: '',
-        payload: {
-            'address': 'Street',
-            //'brandId': localStorage.getItem('brandId'),
-            'brandId': '',
-            'city': 'City',
-            'countryCode': 'GBR',
-            'postcode': 'W1D 3PX'
-        }
-    };
-
-    beforeEach(() => {
-        cy.authenticate();
-        cy.createProgram({ 'name': "Program" + Math.random().toString() }).then((response) => {
-            window.localStorage.setItem('programId', response.body.items[0].id);
+    before(() => {
+        cy.fixture('authData.json').then((data) => {
+            authPayload = data.authPayload
         });
-        cy.createBrand({ 'name': "Brand" + Math.random().toString() }).then((response) => {
-            window.localStorage.setItem('brandId', response.body.items[0].id);
+        cy.fixture('locationData.json').then((data) => {
+            locationRequestData = data.locationRequestData
         });
     });
 
+    beforeEach(() => {
+        cy.authenticate(authPayload);
+
+        cy.createProgram({ 'name': "Program" + Math.random().toString() }).then((response) => {
+            window.localStorage.setItem('programId', response.body.items[0].id);
+        }).then(() => {locationRequestData.pathParam = localStorage.getItem('programId');});
+
+        cy.createBrand({ 'name': "Brand" + Math.random().toString() }).then((response) => {
+            window.localStorage.setItem('brandId', response.body.items[0].id);
+        }).then(() => {locationRequestData.payload.brandId = localStorage.getItem('brandId');});
+    });
+
     it('Should creat a progam and let the inserted location available through get request', () => {
-        cy.createLocation(requestData).then((response) => {
+        cy.createLocation(locationRequestData).then((response) => {
             expect(response.status).to.eq(201);
         })
     });
 
     it('Shoud return 400 - Bad Request - Invalid schema - Missing Address', () => {
-        requestData.pathParam = localStorage.getItem('programId');
-        requestData.payload.brandId = localStorage.getItem('brandId');
-        delete requestData.payload['address'];
-
-        cy.createLocation(requestData).then((response) => {
+        delete locationRequestData.payload['address'];
+        cy.createLocation(locationRequestData).then((response) => {
             expect(response.status).to.eq(400);
             expect(response.body.error.message).to.eq('Invalid schema');
             expect(response.body.error.metadata[0].message).to.eq('should have required property \'address\'');
